a56fce13cc32b8061da4506b15be1913a511b3be
vansuphan <37709216+vansuphan@users.noreply.github.com>
2019-05-26 01:11:57 +0700

    add lesson2

---
+++ lesson2/index.js
@@ -0,0 +1,52 @@
+var generatePassword = require('password-generator');
+
+/*var passwords = generatePassword(8, false);
+console.log(passwords);*/
+
+/*Example with custom validation rules
+Given the pattern regexp can only match a single character you can build a function that generates multiple passwords until you hit one that complies with your rules.
+
+The following example will generate a password with the following requirements
+
+Must contain at least two numbers
+Must contain at least three uppercase letters
+Must contain at least three lowercase letters
+Must contain at least two special characters
+Must NOT contain sequences of two or more repeated characters*/
+
+var maxLength = 18;
+var minLength = 12;
+var uppercaseMinCount = 3;
+var lowercaseMinCount = 3;
+var numberMinCount = 2;
+var specialMinCount = 2;
+var UPPERCASE_RE = /([A-Z])/g;
+var LOWERCASE_RE = /([a-z])/g;
+var NUMBER_RE = /([\d])/g;
+var SPECIAL_CHAR_RE = /([\?\-])/g;
+var NON_REPEATING_CHAR_RE = /([\w\d\?\-])\1{2,}/g;
+ 
+function isStrongEnough(password) {
+  var uc = password.match(UPPERCASE_RE);
+  var lc = password.match(LOWERCASE_RE);
+  var n = password.match(NUMBER_RE);
+  var sc = password.match(SPECIAL_CHAR_RE);
+  var nr = password.match(NON_REPEATING_CHAR_RE);
+  return password.length >= minLength &&
+    !nr &&
+    uc && uc.length >= uppercaseMinCount &&
+    lc && lc.length >= lowercaseMinCount &&
+    n && n.length >= numberMinCount &&
+    sc && sc.length >= specialMinCount;
+}
+ 
+function customPassword() {
+  var password = "";
+  var randomLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
+  while (!isStrongEnough(password)) {
+    password = generatePassword(randomLength, false, /[\w\d\?\-]/);
+  }
+  return password;
+}
+ 
+console.log(customPassword());
